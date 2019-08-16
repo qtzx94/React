2e63f93b8dcb62249a8095834835eea783634826
qtzx <qtzx94@gmail.com>
2019-08-16 09:47:48 +0800

    使用immutable.js管理store中的数据

--- jianshu/src/common/header/store/reducer.js
+++ jianshu/src/common/header/store/reducer.js
@@ -1,20 +1,19 @@
 import * as contants from './constants';
+import { fromJS } from 'immutable';
 
-const defaultState = {
+// immutable对象
+const defaultState = fromJS({
     focused: false 
-};
+});
 
 // reducer.js是一个纯函数
 export default (state = defaultState, action) => {
     if(action.type === contants.SEARCH_FOCUS) {
-        return {
-            focused: true
-        }
+        // immutable对象的set方法，会结合之前的immutable对象的值和设置的值，返回新的对象，不会修改原来的对象
+        return state.set('focused', true)
     }
     if(action.type === contants.SEARCH_BLUR) {
-        const newState = JSON.parse(JSON.stringify(state));
-        newState.focused = false;
-        return newState;
+        return state.set('focused', false)
     }
     return state;
 }
